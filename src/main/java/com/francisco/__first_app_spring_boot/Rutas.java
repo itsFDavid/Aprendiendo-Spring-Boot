20fec645528597e60297b7af896f476e86f31672
itsFDavid <fdavid04@icloud.com>
2024-07-26 19:47:36 -0600

    fisrt commit

---
+++ src/main/java/com/francisco/__first_app_spring_boot/Rutas.java
@@ -0,0 +1,9 @@
+package com.francisco.__first_app_spring_boot;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rutas {
+
+}
---
+++ src/test/java/com/francisco/__first_app_spring_boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.__first_app_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
